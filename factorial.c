5ae8ee2c1195b27188cbdea18daed113dd74046d
Ritikindore <49471472+Ritikindore@users.noreply.github.com>
2019-04-10 13:43:26 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 int main()
 {
-int i, j, row = 5;
-printf("Enter the number of rows: ");
-for(i = 1; i < rows; ++i)
-{
-for(j = 1; j < i; ++j)
-{
-printf(" * ");
-}
-printf(" \n ");
-}
-return 0;
+int c, n, fact = 1;
+printf("Enter the number: ");
+ scanf("%d", &n);
+ for(c = 1; c <= n; c++)
+  fact = fact * c;
+  printf("Factorial of %d = %d\n", n, fact);
+  return 0;
 }
